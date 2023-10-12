bf8d08b5bf3ce029a48dda47e9655f6d0351e5a6
Aleksandr Harisov <bwb-a.harisov@fondespierre.com>
2023-10-12 09:30:12 +0200

    added error management for pdo

--- Model.php
+++ Model.php
@@ -13,8 +13,18 @@ class Model {
         $login = Conf::getLogin();
         $password = Conf::getPassword();
         
-        self::$pdo = new PDO("mysql:host=$hostname;dbname=$database_name", $login, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+        try {
+            
+            self::$pdo = new PDO("mysql:host=$hostname;dbname=$database_name", $login, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+
+            self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+
+            echo $e->getMessage(); // affiche un message d'erreur
+            die();
+        }
     }
+    
 }
 
 Model::Init();
